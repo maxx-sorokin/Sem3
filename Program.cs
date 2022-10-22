3fec56c1e5413979a6e16eb500cd008c32c03be0
Maxim Sorokin <maximka773@mail.ru>
2022-10-22 12:21:48 +0300

    всё

--- Program.cs
+++ Program.cs
@@ -1,2 +1,173 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите номер задачи: 17, 18, 21 или 22");
+int z = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+// // Задача 17 - Напишите программу, которая принимает на вход координаты точки (X и Y),
+// // причём X не равно 0 и Y не равно 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int DetermineNumberOfQuarter(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        return 1;
+    }
+    if (x < 0 && y > 0)
+    {
+        return 2;
+    }
+    if (x < 0 && y < 0)
+    {
+        return 3;
+    }
+    if (x > 0 && y < 0)
+    {
+        return 4;
+    }
+
+    return -1;
+}
+
+if (z == 17)
+{
+    Console.WriteLine("Введите координаты");
+    bool isParsedX = int.TryParse(Console.ReadLine(), out int x);
+    bool isParsedY = int.TryParse(Console.ReadLine(), out int y);
+
+    if (!isParsedX || !isParsedY)
+    {
+        Console.WriteLine("Ошибка! Не правильо введены координаты");
+        return;
+    }
+
+    int numberOfQuarter = DetermineNumberOfQuarter(x, y);
+
+    if (numberOfQuarter == -1)
+    {
+        Console.WriteLine("Ошибка! Не правильо введены координаты");
+    }
+    else
+    {
+        Console.WriteLine($"{numberOfQuarter} четверть");
+    }
+}
+
+
+
+
+
+
+// Задача 18 - Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+string CoodinateOfQuarter(int q)
+{
+    switch (q)
+    {
+        case 1:
+            return "x > 0 и y > 0";
+        case 2:
+            return "x < 0 и y > 0";
+        case 3:
+            return "x < 0 и y < 0";
+        case 4:
+            return "x > 0 и y < 0";
+        default:
+            return "Ошибка! Не правильно задана четверть";
+    }
+}
+
+if (z == 18)
+{
+    Console.WriteLine("Введите номер четверти");
+    bool isParsedQuarter = int.TryParse(Console.ReadLine(), out int quarter);
+
+    if (!isParsedQuarter)
+    {
+        Console.WriteLine("Ошибка! Не правильно задана четверть");
+        return;
+    }
+    string result = CoodinateOfQuarter(quarter);
+
+    Console.WriteLine(result);
+}
+
+
+
+
+
+// Задача 21 - Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+double Distance(int x1, int y1, int x2, int y2)
+{
+    double dis = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    return dis;
+}
+
+if (z == 21)
+{
+    Console.WriteLine("Введите координаты первой точки");
+    bool isParsedXA = int.TryParse(Console.ReadLine(), out int xA);
+    bool isParsedYA = int.TryParse(Console.ReadLine(), out int yA);
+
+    Console.WriteLine("Введите координаты второй точки");
+    bool isParsedXB = int.TryParse(Console.ReadLine(), out int xB);
+    bool isParsedYB = int.TryParse(Console.ReadLine(), out int yB);
+
+    if (!isParsedXA || !isParsedYA || !isParsedXB || !isParsedYB)
+    {
+        Console.WriteLine("Ошибка! Не правильо введены координаты");
+        return;
+    }
+
+    double distanceDot = Distance(xA, yA, xB, yB);
+    Console.WriteLine($"Расстояние между точками {distanceDot}");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Задача 22 - Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+int [] TableOfSquares(int number)
+{
+    int[] array = new int[number];
+
+    for (int i = 0; i < number; i++)
+    {
+        array[i] = (i + 1) * (i + 1);
+    }
+    return array;
+}
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+if (z == 22)
+{
+    Console.WriteLine("Введите число");
+    bool isParsedN = int.TryParse(Console.ReadLine(), out int N);
+
+    if (!isParsedN)
+    {
+        Console.WriteLine("Ошибка! Это не число");
+        return;
+    }
+
+    int [] result = TableOfSquares(N);
+    PrintArray(result);
+}
